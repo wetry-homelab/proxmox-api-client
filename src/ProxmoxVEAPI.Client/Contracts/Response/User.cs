6de621a07ac1fddf8fed59faef2f82f27c7a7c5b
Patrick Grasseels <grasseelsp@hotmail.com>
2021-02-12 15:32:18 +0100

    Add get user list

---
+++ src/ProxmoxVEAPI.Client/Contracts/Response/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProxmoxVEAPI.Client.Contracts.Response
+{
+    public class User
+    {
+        [JsonPropertyName("userid")]
+        public string UserId { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("firstname")]
+        public string Firstname { get; set; }
+
+        [JsonPropertyName("lastname")]
+        public string Lastname { get; set; }
+
+        [JsonPropertyName("enable")]
+        public bool Enable { get; set; }
+    }
+}
--- src/ProxmoxVEAPI.Client/UserClient.cs
+++ src/ProxmoxVEAPI.Client/UserClient.cs
@@ -1,4 +1,5 @@
 ﻿using ProxmoxVEAPI.Client.Contracts.Request;
+using ProxmoxVEAPI.Client.Contracts.Response;
 using System.Threading.Tasks;
 
 namespace ProxmoxVEAPI.Client
@@ -6,10 +7,16 @@ namespace ProxmoxVEAPI.Client
     public class UserClient : BaseClient
     {
         private const string GetTicketUri = "api2/json/access/ticket";
+        private const string GetUserUri = "api2/json/access/users";
 
         public Task<bool> AuthenticateUser(AuthenticateContractRequest request)
         {
             return PostAsSuccessAsync(GetTicketUri, request);
+        }
+
+        public Task<User[]> GetUsers()
+        {
+            return GetAsync<User>(GetUserUri);
         }
     }
 }
