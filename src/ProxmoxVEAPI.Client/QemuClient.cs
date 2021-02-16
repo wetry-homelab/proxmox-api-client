b7d44ae95fb1ce16933f920a6013919d73133a5a
Patrick Grasseels <grasseelsp@hotmail.com>
2021-02-16 08:20:40 +0100

    Add status management

--- src/ProxmoxVEAPI.Client/QemuClient.cs
+++ src/ProxmoxVEAPI.Client/QemuClient.cs
@@ -8,12 +8,32 @@ namespace ProxmoxVEAPI.Client
 
         public Task<bool> RestartQemu(string nodeId, int vmId)
         {
-            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status", null);
+            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status/reboot", null);
         }
 
         public Task<bool> DeleteQemu(string nodeId, int vmId)
         {
             return DeleteAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}");
+        }
+
+        public Task<bool> StopQemu(string nodeId, int vmId)
+        {
+            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status/stop", null);
+        }
+
+        public Task<bool> ShutdownQemu(string nodeId, int vmId)
+        {
+            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status/shutdown", null);
+        }
+
+        public Task<bool> ResumeQemu(string nodeId, int vmId)
+        {
+            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status/resume", null);
+        }
+
+        public Task<bool> StartQemu(string nodeId, int vmId)
+        {
+            return PostAsSuccessAsync($"{BaseQemuUri}/{nodeId}/qemu/{vmId}/status/start", null);
         }
     }
 }
